b2d884e72c8058a36c4e14c8a7c28b178d9c2d5e
rondinelli-oliveira <rondinelliads@gmail.com>
2021-05-06 22:11:05 -0300

    Domain model, database seed

---
+++ back-end/src/main/java/com/devsuperior/dsvendas/domain/model/Seller.java
@@ -0,0 +1,53 @@
+package com.devsuperior.dsvendas.domain.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sellers")
+public class Seller {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+
+	@OneToMany(mappedBy = "seller")
+	private List<Sale> sales = new ArrayList<>();
+
+	public Seller() {
+	}
+
+	public Seller(Long id, String nome) {
+		this.id = id;
+		this.name = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return name;
+	}
+
+	public void setNome(String nome) {
+		this.name = nome;
+	}
+
+	public List<Sale> getSales() {
+		return sales;
+	}
+
+}
